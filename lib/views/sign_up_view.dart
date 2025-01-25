38984fb00863b3168b86ec43f3d5c6edd0bb1cff
Mohamed Ali <a74498722@gmail.com>
2025-01-24 16:29:46 -0800

    add SignUpViewBody to SignUpView for improved layout

--- lib/views/sign_up_view.dart
+++ lib/views/sign_up_view.dart
@@ -1,3 +1,4 @@
+import 'package:auth_flow/widgets/sign_up_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SignUpView extends StatelessWidget {
@@ -5,6 +6,8 @@ class SignUpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SignUpViewBody(),
+    );
   }
 }
